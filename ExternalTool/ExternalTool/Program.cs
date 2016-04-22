8a09fc506b845eed80cb7e9e053b6cb515c23232
Cody Freeman <cxf6084@g.rit.edu>
2016-04-22 16:31:59 -0400

    FINALLY got this map editor working :^)

--- ExternalTool/ExternalTool/Program.cs
+++ ExternalTool/ExternalTool/Program.cs
@@ -14,7 +14,7 @@ namespace ExternalTool
         static void Main(string[] args)
         {
             // array to hold the characters for map creation
-            string[,] map1 = new string[10,10]; // char[9] = 10 characters for the 10 x 10 map 
+            string[,] map1 = new string[10, 10]; // char[9] = 10 characters for the 10 x 10 map 
 
             //directions
             Console.WriteLine("Mascot Mayhem Map Editor");
@@ -33,76 +33,63 @@ namespace ExternalTool
                 for (int j = 0; j < 10; j++)
                 {
                     Console.WriteLine("Type in the map for row " + i + " column " + j); // ex: g, g, g, g, g, g, g, g, g, g
-                    map1[i,j] = Console.ReadLine(); // fills in the typed-in code to the array 
+                    map1[i, j] = Console.ReadLine(); // fills in the typed-in code to the array 
+
+                    if (map1[i, j] == "f")
+                    {
+                        map1[i, j] = "field";
+                    }
+                    else if (map1[i, j] == "r")
+                    {
+                        map1[i, j] = "river";
+                    }
+                    else if (map1[i, j] == "p")
+                    {
+                        map1[i, j] = "pavement";
+                    }
+                    else if (map1[i, j] == "fo")
+                    {
+                        map1[i, j] = "forest";
+                    }
+                    else if (map1[i, j] == "w")
+                    {
+                        map1[i, j] = "win tile";
+                    }
                 }
             }
+                // set up streamwriter
+                Console.WriteLine("\nType in filename"); //custom map names
+                string fileName = Console.ReadLine();
 
-            // set up streamwriter
-            Console.WriteLine("\nType in filename"); //custom map names
-            string fileName = Console.ReadLine(); 
-
-            // try block to handle the filewriting exceptions 
-            try
-            {
-                // create StreamWriter
-                StreamWriter output = new StreamWriter(fileName + ".txt");
-
-                // output array data into the text file 
-                for (int i = 0; i < 10; i++)
+                // try block to handle the filewriting exceptions 
+                try
                 {
-                    for (int j = 0; j < 10; j++)
+                    // create StreamWriter
+                    StreamWriter output = new StreamWriter(fileName + ".txt");
+
+                    // output array data into the text file 
+                    for (int i = 0; i < 10; i++)
                     {
-                        output.WriteLine(map1[i,j]);
+                        output.WriteLine(map1[i, 0] + "," + map1[i, 1] + "," + map1[i, 2] + "," + map1[i, 3] + "," + map1[i, 4] + "," + map1[i, 5] + "," + map1[i, 6] + "," + map1[i, 7] + "," + map1[i, 8] + "," + map1[i, 9]);
                     }
+
+                    // close the streamwriter
+                    output.Close();
                 }
-                
-                // close the streamwriter
-                output.Close(); 
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace); 
-            }
-
-            // test - replace file characters with full words
-            // ex: "f" becomes "field"
-            // idea is to speed up map creation and allow faster creations of larger or more complex maps 
-
-            // while loop to read through array 
-            for(int i = 0; i < 10; i++)
-            {
-               for(int j = 0; j < 10; j++)
+                catch (Exception ex)
                 {
-                    if(map1[i,j] == "f")
-                    {
-                        map1[i, j] = "field"; 
-                    }
-                    else if(map1[i,j] == "r")
-                    {
-                        map1[i, j] = "river"; 
-                    }
-                    else if(map1[i,j] == "p")
-                    {
-                        map1[i, j] = "pavement"; 
-                    }
-                    else if(map1[i,j] == "fo")
-                    {
-                        map1[i, j] = "forest"; 
-                    }
-                    else if(map1[i,j] == "w")
-                    {
-                        map1[i, j] = "win tile"; 
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid tile type found at " + i + " " + j);
-                        Console.WriteLine("Please edit and try again");
-                        return; 
-                    }
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
                 }
+
+                // test - replace file characters with full words
+                // ex: "f" becomes "field"
+                // idea is to speed up map creation and allow faster creations of larger or more complex maps 
+
+
+                Console.WriteLine("The file has been saved, it can be located in the bin/debug folder");
             }
-            Console.WriteLine("The file has been saved, it can be located in the bin/debug folder");
         }
     }
-}
+
+
